9a84e97db2b58cccf7a12e5988b6e0015cd2f86e
Xin Liu <sam@secondstate.io>
2022-12-05 19:50:08 +0800

    feat(memory): add new example
    Signed-off-by: Xin Liu <sam@secondstate.io>

---
+++ manipulate-linear-memory/README.md
@@ -0,0 +1,62 @@
+# Define & Run Host Function
+
+This example demonstrates how to manipulate linear memory.
+
+Now let's build and run this example.
+
+- Install `rustup` and `Rust`
+
+  Go to the [official Rust webpage](https://www.rust-lang.org/tools/install) and follow the instructions to install `rustup` and `Rust`.
+
+  > It is recommended to use Rust 1.63 or above in the stable channel.
+
+- Install `libwasmedge`
+
+  Refer to the [Quick Install](https://wasmedge.org/book/en/quick_start/install.html#quick-install) section of WasmEdge Runtime Book to install `libwasmedge`. Or, use the following command directly
+
+  ```bash
+  // The command will create a directory `$HOME/.wasmedge`
+  ./install_libwasmedge.sh
+
+  source $HOME/.wasmedge/env
+  ```
+
+  > For users in China mainland (中国大陆地区), try the following command to install `libwasmedge` if failed to run the command above
+  >
+  > ```bash
+  > ./install_libwasmedge_zh.sh
+  > source $HOME/.wasmedge/env
+  > ```
+
+  > To install a specific version of `libwasmedge`, use `-v` option. For example, the following command installs `libwasmedge 0.11.2` to `$HOME/.wasmedge`
+  >
+  > ```bash
+  > ./install_libwasmedge.sh -v 0.11.2
+  > source $HOME/.wasmedge/env
+  > ```
+
+- Download example
+
+  ```bash
+  git clone git@github.com:apepkuss/wasmedge-rust-examples.git
+  cd wasmedge-rust-examples/manipulate-linear-memory
+  ```
+
+- Build & run
+
+  ```bash
+  cargo run
+  ```
+
+  If the command runs successfully, then the following result is printed out on the screen:
+
+  ```bash
+  The memory size (in pages): 1
+  The data size (in bytes): 65536
+  The memory size (in pages) after growing memory additional 2 pages: 3
+  The data size (in bytes) after growing memory additional 2 pages: 196608
+  Set 0xFEFEFFE at the memory address 0x2220
+  Retrieve the value at the memory address 0x2220: 0xFEFEFFE
+  Set 0xFEA09 at the memory address 0x1FFFC
+  Retrieve the value at the memory address 0x1FFFC: 0xFEA09
+  ```
---
+++ manipulate-linear-memory/src/main.rs
@@ -0,0 +1,81 @@
+use wasmedge_sdk::{params, wat2wasm, WasmVal, Vm};
+
+#[cfg_attr(test, test)]
+fn main() -> anyhow::Result<()> {
+    let wasm_bytes = wat2wasm(
+        r#"
+        (module
+        (type $mem_size_t (func (result i32)))
+        (type $get_at_t (func (param i32) (result i32)))
+        (type $set_at_t (func (param i32) (param i32)))
+
+        (memory $mem 1)
+
+        (func $get_at (type $get_at_t) (param $idx i32) (result i32)
+            (i32.load (local.get $idx)))
+
+        (func $set_at (type $set_at_t) (param $idx i32) (param $val i32)
+            (i32.store (local.get $idx) (local.get $val)))
+
+        (func $mem_size (type $mem_size_t) (result i32)
+            (memory.size))
+
+        (export "get_at" (func $get_at))
+        (export "set_at" (func $set_at))
+        (export "mem_size" (func $mem_size))
+        (export "memory" (memory $mem)))
+        "#
+        .as_bytes(),
+    )?;
+
+    // create a Vm instance and register the module into it
+    let vm = Vm::new(None)?.register_module_from_bytes("extern", &wasm_bytes)?;
+    
+    // get the module instance named "extern"
+    let extern_instance = vm.named_module("extern")?;
+
+    // get the exported memory instance named "memory"
+    let mut memory = extern_instance
+        .memory("memory")
+        .ok_or_else(|| anyhow::anyhow!("failed to get memory instance named 'memory'"))?;
+
+    // check memory size
+    println!("The memory size (in pages): {}", memory.size());
+    println!("The data size (in bytes): {}", memory.data_size());
+
+    // grow memory size
+    memory.grow(2)?;
+    println!("The memory size (in pages) after growing memory additional 2 pages: {}", memory.size());
+    println!("The data size (in bytes) after growing memory additional 2 pages: {}", memory.data_size());
+
+    // get the exported functions: "set_at" and "get_at"
+    let set_at = extern_instance
+        .func("set_at")
+        .ok_or_else(|| anyhow::Error::msg("Not found exported function named 'set_at'."))?;
+    let get_at = extern_instance
+        .func("get_at")
+        .ok_or_else(|| anyhow::Error::msg("Not found exported function named 'get_at`."))?;
+
+    // call the exported function named "set_at"
+    let mem_addr = 0x2220;
+    let val = 0xFEFEFFE;
+    println!("Set {:#X} at the memory address {:#X}", val, mem_addr);
+    set_at.call(&vm, params!(mem_addr, val))?;
+
+    // call the exported function named "get_at"
+    let returns = get_at.call(&vm, params!(mem_addr))?;
+    println!("Retrieve the value at the memory address {:#X}: {:#X}", mem_addr, returns[0].to_i32());
+
+    // call the exported function named "set_at"
+    let page_size = 0x1_0000;
+    let mem_addr = (page_size * 2) - std::mem::size_of_val(&val) as i32;
+    let val = 0xFEA09;
+    println!("Set {:#X} at the memory address {:#X}", val, mem_addr);
+    set_at.call(&vm, params!(mem_addr, val))?;
+
+    // call the exported function named "get_at"
+    let returns = get_at.call(&vm, params!(mem_addr))?;
+    println!("Retrieve the value at the memory address {:#X}: {:#X}", mem_addr, returns[0].to_i32());
+
+    Ok(())
+}
