629df4cb72de22b9b939d01ddc104ed30253b53f
Xin Liu <sam@secondstate.io>
2023-07-28 09:38:21 +0000

    chore(wasm-lib): update dependency and code
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- call-wasm-lib-from-host/call-wasm-lib/src/main.rs
+++ call-wasm-lib-from-host/call-wasm-lib/src/main.rs
@@ -11,7 +11,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = ConfigBuilder::default().build()?;
 
     // create a VM with the config
-    let vm = VmBuilder::new().with_config(config).build::<NeverType>()?;
+    let vm = VmBuilder::new().with_config(config).build()?;
 
     let res = vm
         .register_module_from_file("wasm-lib", &wasm_lib_file)?
