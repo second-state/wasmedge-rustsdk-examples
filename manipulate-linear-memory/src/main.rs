06307e30745964788586e4c2bd37d679ae5cd2be
Xin Liu <sam@secondstate.io>
2023-07-30 14:24:38 +0000

    chore(memory): update dependency and code
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- manipulate-linear-memory/src/main.rs
+++ manipulate-linear-memory/src/main.rs
@@ -1,4 +1,4 @@
-use wasmedge_sdk::{params, wat2wasm, NeverType, VmBuilder, WasmVal};
+use wasmedge_sdk::{params, wat2wasm, VmBuilder, WasmVal};
 
 #[cfg_attr(test, test)]
 fn main() -> anyhow::Result<()> {
@@ -30,7 +30,7 @@ fn main() -> anyhow::Result<()> {
 
     // create a Vm instance and register the module into it
     let mut vm = VmBuilder::new()
-        .build::<NeverType>()?
+        .build()?
         .register_module_from_bytes("extern", &wasm_bytes)?;
 
     // get the module instance named "extern"
