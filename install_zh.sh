1e4e621146426dbb3f4e615030196c61cceaaf94
Xin Liu <sam@secondstate.io>
2023-09-11 04:40:42 +0000

    chore: update install and uninstall script
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- install_zh.sh
+++ install_zh.sh
@@ -45,7 +45,7 @@ main() {
     fi
 
     if [ "$INSTALL_PY_URL" = "" ]; then
-        INSTALL_PY_URL="https::/ghproxy.com/https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.py"
+        INSTALL_PY_URL="https://ghproxy.com/https://raw.githubusercontent.com/WasmEdge/WasmEdge/master/utils/install.py"
     fi
 
     if command -v curl &>/dev/null; then
