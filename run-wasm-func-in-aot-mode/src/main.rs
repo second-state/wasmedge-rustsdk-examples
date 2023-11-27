bb8768c87780d13607f51f26b42dee99f183ab2f
zzz <458761603@qq.com>
2023-11-27 08:35:19 +0000

    Fix: Fix fib.wat
    Signed-off-by: zzz <458761603@qq.com>

--- run-wasm-func-in-aot-mode/src/main.rs
+++ run-wasm-func-in-aot-mode/src/main.rs
@@ -16,24 +16,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             (func $fib (param $n i32) (result i32)
              (if
               (i32.lt_s
-               (get_local $n)
+               (local.get $n)
                (i32.const 2)
               )
-              (return
-               (i32.const 1)
+              (then
+               (return (i32.const 1))
               )
              )
              (return
               (i32.add
                (call $fib
                 (i32.sub
-                 (get_local $n)
+                 (local.get $n)
                  (i32.const 2)
                 )
                )
                (call $fib
                 (i32.sub
-                 (get_local $n)
+                 (local.get $n)
                  (i32.const 1)
                 )
                )
