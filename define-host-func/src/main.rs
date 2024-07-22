8e1c0b1823f6b119f7eac00cc9d173b42ba5c557
csh <458761603@qq.com>
2024-07-22 17:05:36 +0800

    chore(define-host-func): update to `wasmedge-sys v0.14.0`
    Signed-off-by: csh <458761603@qq.com>

--- define-host-func/src/main.rs
+++ define-host-func/src/main.rs
@@ -1,27 +1,39 @@
+use std::collections::HashMap;
+
 use wasmedge_sdk::{
-    error::HostFuncError, host_function, params, Caller, ImportObjectBuilder, NeverType, ValType,
-    VmBuilder, WasmVal, WasmValue,
+    error::CoreError, params, AsInstance, CallingFrame, ImportObjectBuilder, Instance, Store,
+    ValType, Vm, WasmVal, WasmValue,
 };
 
-#[host_function]
-fn my_add(_caller: Caller, input: Vec<WasmValue>) -> Result<Vec<WasmValue>, HostFuncError> {
+fn my_add(
+    _: &mut (),
+    _inst: &mut Instance,
+    _caller: &mut CallingFrame,
+    input: Vec<WasmValue>,
+) -> Result<Vec<WasmValue>, CoreError> {
     // check the number of inputs
     if input.len() != 2 {
-        return Err(HostFuncError::User(1));
+        return Err(CoreError::Execution(
+            wasmedge_sdk::error::CoreExecutionError::FuncSigMismatch,
+        ));
     }
 
     // parse the first input of WebAssembly value type into Rust built-in value type
     let a = if input[0].ty() == ValType::I32 {
         input[0].to_i32()
     } else {
-        return Err(HostFuncError::User(2));
+        return Err(CoreError::Execution(
+            wasmedge_sdk::error::CoreExecutionError::FuncSigMismatch,
+        ));
     };
 
     // parse the second input of WebAssembly value type into Rust built-in value type
     let b = if input[1].ty() == ValType::I32 {
         input[1].to_i32()
     } else {
-        return Err(HostFuncError::User(3));
+        return Err(CoreError::Execution(
+            wasmedge_sdk::error::CoreExecutionError::FuncSigMismatch,
+        ));
     };
 
     let c = a + b;
@@ -29,20 +41,22 @@ fn my_add(_caller: Caller, input: Vec<WasmValue>) -> Result<Vec<WasmValue>, Host
     Ok(vec![WasmValue::from_i32(c)])
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     // create an import module
-    let import = ImportObjectBuilder::new()
-        .with_func::<(i32, i32), i32, NeverType>("add", my_add, None)?
-        .build::<NeverType>("extern", None)?;
+
+    let mut import_builder = ImportObjectBuilder::new("extern", ()).unwrap();
+    import_builder
+        .with_func::<(i32, i32), i32>("add", my_add)
+        .unwrap();
+    let mut import_object = import_builder.build();
+
+    let mut instances = HashMap::new();
+    instances.insert(import_object.name().unwrap(), &mut import_object);
 
     // create a new Vm with default config
-    let mut vm = VmBuilder::new().build()?;
+    let mut vm = Vm::new(Store::new(None, instances).unwrap());
 
-    vm.register_import_module(&import)?;
-
-    let res = vm.run_func(Some("extern"), "add", params!(15, 51))?;
+    let res = vm.run_func(Some("extern"), "add", params!(15, 51)).unwrap();
 
     println!("add({}, {}) = {}", 15, 51, res[0].to_i32());
-
-    Ok(())
 }
