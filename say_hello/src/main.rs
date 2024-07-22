8cdf307dfc1e8daa3525e9b99c19bc928e335ca6
csh <458761603@qq.com>
2024-07-22 17:09:55 +0800

    chore(say_hello): update to `wasmedge-sys v0.14.0`
    Signed-off-by: csh <458761603@qq.com>

--- say_hello/src/main.rs
+++ say_hello/src/main.rs
@@ -1,10 +1,15 @@
+use std::collections::HashMap;
+
 use wasmedge_sdk::{
-    error::HostFuncError, host_function, params, Caller, ImportObjectBuilder, NeverType, VmBuilder,
-    WasmValue,
+    error::CoreError, params, CallingFrame, ImportObjectBuilder, Instance, Store, Vm, WasmValue,
 };
 
-#[host_function]
-fn hello(_caller: Caller, _args: Vec<WasmValue>) -> Result<Vec<WasmValue>, HostFuncError> {
+fn hello(
+    _data: &mut (),
+    _inst: &mut Instance,
+    _frame: &mut CallingFrame,
+    _args: Vec<WasmValue>,
+) -> Result<Vec<WasmValue>, CoreError> {
     println!("Hello, world!");
 
     Ok(vec![])
@@ -12,15 +17,16 @@ fn hello(_caller: Caller, _args: Vec<WasmValue>) -> Result<Vec<WasmValue>, HostF
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // create a new WasmEdge Vm instance
-    let mut vm = VmBuilder::new().build()?;
 
     // create an import_object module with the host function
-    let import = ImportObjectBuilder::new()
-        .with_func::<(), (), NeverType>("say_hello", hello, None)?
-        .build::<NeverType>("extern", None)?;
+    let mut builder = ImportObjectBuilder::new("extern", ())?;
+    builder.with_func::<(), ()>("say_hello", hello)?;
+    let mut import = builder.build();
 
-    // register the import module into vm
-    vm.register_import_module(&import)?;
+    let mut instances = HashMap::new();
+    instances.insert("extern".into(), &mut import);
+    let store = Store::new(None, instances)?;
+    let mut vm = Vm::new(store);
 
     let _ = vm.run_func(Some("extern"), "say_hello", params!())?;
 
