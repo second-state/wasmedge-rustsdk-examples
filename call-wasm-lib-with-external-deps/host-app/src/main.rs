76567ce6ddd82901ba564b95aead103a94635573
csh <458761603@qq.com>
2024-07-22 15:36:13 +0800

    chore(call-wasi-lib-with-external-deps): update to `wasmedge-sdk 0.14.0`
    Signed-off-by: csh <458761603@qq.com>

--- call-wasm-lib-with-external-deps/host-app/src/main.rs
+++ call-wasm-lib-with-external-deps/host-app/src/main.rs
@@ -1,29 +1,39 @@
+use std::collections::HashMap;
+
 use wasmedge_sdk::{
-    config::{CommonConfigOptions, ConfigBuilder, HostRegistrationConfigOptions},
-    error::HostFuncError,
-    host_function, params, Caller, ImportObjectBuilder, NeverType, ValType, VmBuilder, WasmVal,
-    WasmValue,
+    error::CoreError, params, vm::SyncInst, wasi::WasiModule, AsInstance, CallingFrame,
+    ImportObjectBuilder, Instance, Module, Store, ValType, Vm, WasmVal, WasmValue,
 };
 
-#[host_function]
-fn my_add(_caller: Caller, input: Vec<WasmValue>) -> Result<Vec<WasmValue>, HostFuncError> {
+fn my_add(
+    _: &mut (),
+    _inst: &mut Instance,
+    _caller: &mut CallingFrame,
+    input: Vec<WasmValue>,
+) -> Result<Vec<WasmValue>, CoreError> {
     // check the number of inputs
     if input.len() != 2 {
-        return Err(HostFuncError::User(1));
+        return Err(CoreError::Execution(
+            wasmedge_sdk::error::CoreExecutionError::FuncSigMismatch,
+        ));
     }
 
     // parse the first input of WebAssembly value type into Rust built-in value type
     let a = if input[0].ty() == ValType::I32 {
         input[0].to_i32()
     } else {
-        return Err(HostFuncError::User(2));
+        return Err(CoreError::Execution(
+            wasmedge_sdk::error::CoreExecutionError::FuncSigMismatch,
+        ));
     };
 
     // parse the second input of WebAssembly value type into Rust built-in value type
     let b = if input[1].ty() == ValType::I32 {
         input[1].to_i32()
     } else {
-        return Err(HostFuncError::User(3));
+        return Err(CoreError::Execution(
+            wasmedge_sdk::error::CoreExecutionError::FuncSigMismatch,
+        ));
     };
 
     let c = a + b;
@@ -31,7 +41,7 @@ fn my_add(_caller: Caller, input: Vec<WasmValue>) -> Result<Vec<WasmValue>, Host
     Ok(vec![WasmValue::from_i32(c)])
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let args: Vec<String> = std::env::args().collect();
     println!("args: {:?}", args);
 
@@ -39,23 +49,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let num1: i32 = args[2].parse().unwrap();
     let num2: i32 = args[3].parse().unwrap();
 
+    let mut wasi_module = WasiModule::create(None, None, None).unwrap();
+
     // create an import module
-    let import = ImportObjectBuilder::new()
-        .with_func::<(i32, i32), i32, NeverType>("real_add", my_add, None)?
-        .build::<NeverType>("my_math_lib", None)?;
+    let mut import_builder = ImportObjectBuilder::new("my_math_lib", ()).unwrap();
+    import_builder
+        .with_func::<(i32, i32), i32>("real_add", my_add)
+        .unwrap();
+    let mut import_object = import_builder.build();
 
-    // create a new Vm with default config
-    let config = ConfigBuilder::new(CommonConfigOptions::default())
-        .with_host_registration_config(HostRegistrationConfigOptions::default().wasi(true))
-        .build()?;
+    let mut instances: HashMap<String, &mut dyn SyncInst> = HashMap::new();
+    instances.insert(wasi_module.name().to_string(), wasi_module.as_mut());
+    instances.insert(import_object.name().unwrap(), &mut import_object);
 
-    let mut vm = VmBuilder::new().with_config(config).build()?;
-    vm.register_import_module(&import)?;
+    let mut vm = Vm::new(Store::new(None, instances).unwrap());
+
+    let module = Module::from_file(None, wasm_lib_file).unwrap();
+
+    vm.register_module(Some("extern"), module).unwrap();
+
     let res = vm
-        .register_module_from_file("extern", wasm_lib_file)?
-        .run_func(Some("extern"), "add", params!(num1, num2))?;
+        .run_func(Some("extern"), "add", params!(num1, num2))
+        .unwrap();
 
     println!("add({}, {}) = {}", num1, num2, res[0].to_i32());
-
-    Ok(())
 }
